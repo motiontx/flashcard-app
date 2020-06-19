83d93d922b0039f74b1f80ddebef4fec81f01341
Vittorio Retrivi <retrovitto@gmail.com>
2020-06-19 17:36:25 -0300

    Update client side
    Added Vuetify

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -9,10 +9,13 @@ import VueAxios from 'vue-axios'
 Vue.use(VueAxios, axios)
 axios.defaults.baseURL = 'http://localhost:3000/api';
 
+import vuetify from './plugins/vuetify' // Vuetify
+
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
