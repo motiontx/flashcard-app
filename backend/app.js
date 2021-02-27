6cddcecb90e822eb6ff76b4d9509a915de661a9f
Vittorio Retrivi <retrovitto@gmail.com>
2021-02-27 18:26:04 -0300

    Switched from local mongoDB to Atlas

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,9 @@
 const express = require('express'); // Express
 const app = express();
 
+const dotenv = require('dotenv'); // Environment Variables
+dotenv.config();
+
 const morgan = require('morgan'); // Morgan
 app.use(morgan('dev'));
 
@@ -10,8 +13,12 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })) //application/x-www-form-urlencoded
 
-const mongoose = require('mongoose'); // Mongoose
-mongoose.connect('mongodb://localhost:27017/flashcard-app', {
+const mongoose = require('mongoose');// Mongoose
+const USER = process.env['DATABASE_USER'];
+const DBCLUSTER = process.env['DATABASE_CLUSTER'];
+const DBNAME = process.env['DATABASE_NAME'];
+const DBPASSWORD = process.env['DATABASE_PASSWORD'];
+mongoose.connect(`mongodb+srv://${USER}:${DBPASSWORD}@${DBCLUSTER}.mongodb.net/${DBNAME}?retryWrites=true&w=majority`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
